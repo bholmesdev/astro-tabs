8dfd3f2589e3d4ce7d79b0ae9823988fdb741b1f
bholmesdev <hey@bholmes.dev>
2022-07-05 10:57:08 -0400

    chore: remove vitest config

--- vitest.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vitest/config'
-import preactPlugin from '@preact/preset-vite'
-
-export default defineConfig({
-  plugins: [preactPlugin()],
-  test: {
-    dir: 'test',
-  }
-})
